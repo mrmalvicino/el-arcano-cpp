39c9b8c378cc17dd802361f50a2d22273f4fa23b
mrmalvicino <mrmalvicino@gmail.com>
2024-10-02 11:06:50 -0300

    Prophecies added

--- README.md
+++ README.md
@@ -21,6 +21,24 @@ Luego seguirá el jugador que esté a su derecha y así sucesivamente en sentido
 &nbsp;
 El jugador que primero logre conseguir los recursos asignados será quien cumpla su profecía y gane la partida.
 
+### Profecías
+
+- **Profecía del amor:** Tu profecía es descubrir el enigma del amor profundo, reúne a la carta del Enamorado con la carta del Sol y el poder de tres cuarzos rosas. Las fuerzas del tarot se manifestarán, desvelando el camino hacia la plenitud y la sabiduría interior.
+
+- **Profecía del éxito:** Tu profecía para alcanzar el éxito es alinear la carta La rueda de la fortuna con El carro, y reforzar tu camino con dos Ágatas y una Jade. Estas energías cósmicas se sincronizarán, revelando el sendero hacia el triunfo y la prosperidad.
+
+- **Profecía del mal:** Tu profecía del mal advierte que el destino se entrelazará cuando las cartas de La Muerte y El Diablo se alineen, cargando el aire de tensiones oscuras protege tu espíritu con una turmalina negra. Los vientos del caos soplarán fuerte, pero solo al enfrentarlos, sabrás las lecciones ocultas en las sombras.
+
+- **Profecía de la realeza:** Tu destino está marcado por la realeza, alinea las cartas de La Sacerdotisa, La Emperatriz y El Emperador. Refuerza tu poder con dos amatistas y una piedra de luna. El equilibrio y la sabiduría reinarán en tu camino.
+
+- **Profecía de la transformación:** Tu profecía es la llave para una transformación trascendental. Une la carta del Mago, La Estrella, El Colgado y El Papa junto con la piedra de Jade para desatar una revelación profunda. La habilidad mágica y la inspiración iluminarán tu camino, mientras que la introspección y el sacrificio te brindarán una nueva perspectiva.
+
+- **Profecía de la oscuridad:** El enigma de la oscuridad se despliega ante ti, donde la Torre se alza majestuosa y el Ermitaño guarda los secretos del abismo. Unifica tus fuerzas con el poder de 3 sodalitas. En el abrazo de la oscuridad, tu voluntad se forjará con claridad y dominio sobre tu destino.
+
+- **Profecía del poder:** El poder te espera, latente y majestuoso uní las fuerzas de La Justicia, El Juicio y La Fuerza. Refuerza tu voluntad con dos ojos de tigre. El equilibrio, la verdad y la valentía te guiarán hacia el dominio de tu destino.
+
+- **Profecía de la libertad:** La libertad se manifiesta en tu camino, conecta las energías de El Mundo y El Loco, y fortalece tu esencia con tres citrinos. Las puertas hacia lo desconocido se abrirán, guiándote hacia la verdadera emancipación.
+
 ### Dinámica de cada turno
 
 &nbsp;
--- data/initialData.h
+++ data/initialData.h
@@ -75,7 +75,7 @@ void generateEffectsData()
 
     effect.setId(5);
     effect.setName("Bendición del mago");
-    effect.setDescription("El jugador puede realizar una jugada especial, intercambiando dos de sus cartas por dos del mazo ó moviendo cualquier jugador a un casillero específico.");
+    effect.setDescription("El jugador puede realizar una jugada especial, intercambiando dos de sus cartas (a elección) por dos del mazo (al azar) ó moviendo cualquier jugador a un casillero específico.");
     effectsArchive.write(effect);
 
     effect.setId(6);
@@ -129,8 +129,43 @@ void generatePropheciesData()
     Prophecy prophecy;
 
     prophecy.setId(1);
+    prophecy.setName("Profecía del amor");
+    prophecy.setDescription("Tu profecía es descubrir el enigma del amor profundo, reúne a la carta del Enamorado con la carta del Sol y el poder de tres cuarzos rosas. Las fuerzas del tarot se manifestarán, desvelando el camino hacia la plenitud y la sabiduría interior.");
+    propheciesArchive.write(prophecy);
+
+    prophecy.setId(2);
     prophecy.setName("Profecía del éxito");
     prophecy.setDescription("Tu profecía para alcanzar el éxito es alinear la carta La rueda de la fortuna con El carro, y reforzar tu camino con dos Ágatas y una Jade. Estas energías cósmicas se sincronizarán, revelando el sendero hacia el triunfo y la prosperidad.");
+    propheciesArchive.write(prophecy);
+
+    prophecy.setId(3);
+    prophecy.setName("Profecía del mal");
+    prophecy.setDescription("Tu profecía del mal advierte que el destino se entrelazará cuando las cartas de La Muerte y El Diablo se alineen, cargando el aire de tensiones oscuras protege tu espíritu con una turmalina negra. Los vientos del caos soplarán fuerte, pero solo al enfrentarlos, sabrás las lecciones ocultas en las sombras.");
+    propheciesArchive.write(prophecy);
+
+    prophecy.setId(4);
+    prophecy.setName("Profecía de la realeza");
+    prophecy.setDescription("Tu destino está marcado por la realeza, alinea las cartas de La Sacerdotisa, La Emperatriz y El Emperador. Refuerza tu poder con dos amatistas y una piedra de luna. El equilibrio y la sabiduría reinarán en tu camino.");
+    propheciesArchive.write(prophecy);
+
+    prophecy.setId(5);
+    prophecy.setName("Profecía de la transformación");
+    prophecy.setDescription("Tu profecía es la llave para una transformación trascendental. Une la carta del Mago, La Estrella, El Colgado y El Papa junto con la piedra de Jade para desatar una revelación profunda. La habilidad mágica y la inspiración iluminarán tu camino, mientras que la introspección y el sacrificio te brindarán una nueva perspectiva.");
+    propheciesArchive.write(prophecy);
+
+    prophecy.setId(6);
+    prophecy.setName("Profecía de la oscuridad");
+    prophecy.setDescription("El enigma de la oscuridad se despliega ante ti, donde la Torre se alza majestuosa y el Ermitaño guarda los secretos del abismo. Unifica tus fuerzas con el poder de 3 sodalitas. En el abrazo de la oscuridad, tu voluntad se forjará con claridad y dominio sobre tu destino.");
+    propheciesArchive.write(prophecy);
+
+    prophecy.setId(7);
+    prophecy.setName("Profecía del poder");
+    prophecy.setDescription("El poder te espera, latente y majestuoso uní las fuerzas de La Justicia, El Juicio y La Fuerza. Refuerza tu voluntad con dos ojos de tigre. El equilibrio, la verdad y la valentía te guiarán hacia el dominio de tu destino.");
+    propheciesArchive.write(prophecy);
+
+    prophecy.setId(8);
+    prophecy.setName("Profecía de la libertad");
+    prophecy.setDescription("La libertad se manifiesta en tu camino, conecta las energías de El Mundo y El Loco, y fortalece tu esencia con tres citrinos. Las puertas hacia lo desconocido se abrirán, guiándote hacia la verdadera emancipación.");
     propheciesArchive.write(prophecy);
 }
 
